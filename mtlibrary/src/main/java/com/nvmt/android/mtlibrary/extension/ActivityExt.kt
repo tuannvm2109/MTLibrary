b53334c7a0cec49645771f68a34dd3a55ba556cf
tuannvm2109 <nguyenvanmanhtuan@gmail.com>
2021-01-18 11:43:29 +0700

    Them setup status bar + navigation bar

--- mtlibrary/src/main/java/com/nvmt/android/mtlibrary/extension/ActivityExt.kt
+++ mtlibrary/src/main/java/com/nvmt/android/mtlibrary/extension/ActivityExt.kt
@@ -4,9 +4,11 @@ import android.Manifest
 import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Color
 import android.net.Uri
 import android.os.Build
 import android.view.View
+import android.view.WindowManager
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.ListPopupWindow
@@ -214,4 +216,24 @@ fun Activity.showDialogListAnchor(
         listPopupWindow.dismiss()
     }
     listPopupWindow.show()
+}
+
+fun Activity.setupStatusBarAndNavigationBar() {
+    // translucent status bar but not navigation bar
+
+    window.setFlags(
+        WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+        WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+    )
+
+    window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+    window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+    window.statusBarColor = Color.TRANSPARENT
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+        window.setDecorFitsSystemWindows(false)
+    } else {
+        window.decorView.systemUiVisibility =
+            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+    }
 }
